54b8baf0eae29cd576dba624934b71dad916c63c
hirzidevs <hirzidevs@gmail.com>
2024-06-13 11:13:57 +0700

    ReBalancing Default Ore Generator

--- scripts/config.js
+++ scripts/config.js
@@ -19,16 +19,16 @@ If the block you entered does not appear, maybe you have entered an invalid bloc
 you can check the block name by using the command "/setblock" in game
 */
 config.blocks = [
-    { name: "cobblestone", chance: 0.2 },
-    { name: "coal_ore", chance: 0.15 },
-    { name: "iron_ore", chance: 0.125 },
-    { name: "gold_ore", chance: 0.125 },
-    { name: "redstone_ore", chance: 0.1 },
-    { name: "lapis_ore", chance: 0.1 },
-    { name: "copper_ore", chance: 0.1 },
-    { name: "diamond_ore", chance: 0.05 },
-    { name: "emerald_ore", chance: 0.045 },
-    { name: "ancient_debris", chance: 0.005 },
+    { name: "cobblestone", chance: 0.14 },
+    { name: "coal_ore", chance: 0.13 },
+    { name: "iron_ore", chance: 0.13 },
+    { name: "copper_ore", chance: 0.12 },
+    { name: "gold_ore", chance: 0.11 },
+    { name: "redstone_ore", chance: 0.11 },
+    { name: "lapis_ore", chance: 0.11 },
+    { name: "diamond_ore", chance: 0.1 },
+    { name: "emerald_ore", chance: 0.04 },
+    { name: "ancient_debris", chance: 0.01 },
 ]
 
 
